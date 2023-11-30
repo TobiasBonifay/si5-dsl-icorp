500704ac2b1ac5aa63c883b6ac25b4195dc8754b
Mathieu Schalkwijk <mathieu.schalkwijk@etu.unice.fr>
2023-11-30 20:08:00 +0100

    change sc4

--- embeddeds/groovy/GroovuinoML/scripts/sc4.groovy
+++ embeddeds/groovy/GroovuinoML/scripts/sc4.groovy
@@ -8,8 +8,8 @@ state "off" means "buzzer" becomes "low" and "led" becomes "low"
 
 initial "off"
 
-from "on1" to "on2" when "button" becomes "high" and state is "on1"
-from "on2" to "off" when "button" becomes "high" and state is "on2"
+from "on1" to "on2" when "button" becomes "high"
+from "on2" to "off" when "button" becomes "high"
 from "off" to "on1" when "button" becomes "high"
 
 export "Switch!"
